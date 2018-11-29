eec87d72fc9feb4a0784c41a6c1b1badb0e1c457
Benja <ebaq@estrategas.mx>
2018-11-29 16:00:22 -0600

    Initial commit

---
+++ www/scripts/incidencia.js
@@ -0,0 +1,275 @@
+﻿// Si quiere una introducción sobre la plantilla En blanco, vea la siguiente documentación:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Para depurar código al cargar la página en dispositivos/emuladores Ripple o Android: inicie la aplicación, establezca puntos de interrupción 
+// y ejecute "window.location.reload()" en la Consola de JavaScript.
+(function () {
+    "use strict";
+    var myDB;
+    var msg; 
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Controlar la pausa de Cordova y reanudar eventos
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
+        document.getElementById("tipoincidencia").selectedIndex = "0";
+        document.getElementById("btnGuardar").addEventListener('click', RegistrarUsuario, false);
+        //sqlite
+
+        sqlite()//arranco para sacar el id y la lat y lng 
+    };
+
+    function onPause() {
+        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
+    };
+
+    function onResume() {
+        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
+    };
+
+    //FUNCIONES PERSONALIZADAS
+    function BuscarUsuario() {
+        var pFullname = document.getElementById("txtNombre").value;
+        if (pFullname == "") {
+            $("#divResultado").html("Ingrese el nombre de usuario!");
+        } else {
+            var cadena = "<table data-role='table' class='ui-responsive' width='100%'>";
+            cadena = cadena + "<thead><tr><th>Usuario</th><th>Nombre</th><th>Fecha Nac.</th></tr></thead><tbody>";
+
+            //agregando evento Ajax
+            $.ajax({
+                type: "GET",
+                url: "https://implementta.net/andro/AppIncidencias.aspx?query=select%20top%20%201%20*from[directory]",
+                crossDomain: true,
+                cache: false,
+                contentType: "application/json; charset=utf-8",
+                async: false,
+                dataType: "JSON",
+                success: function (result) {
+                    $.each(result, function (i, field) {
+                        cadena = cadena + "<tr>" + "<td>" + field.id + "</td><td>" + field.fullname + "</td><td>" + field.phone + "</td></tr>";
+                    });
+                    cadena = cadena + "</table>";
+                    $("#divLista").append(cadena);
+                },
+                error: function (result) {
+                    alert("Ocurrió un problema. Por favor Comuníquese con el administrador del sistema. Gracias.");
+                }
+            });
+        }
+    }
+
+    function RegistrarUsuario() {
+
+ 
+
+        var pnombre = document.getElementById("txtnombre").value;
+        var ptelefono = document.getElementById("txttelefono").value;
+        var pcorreo = document.getElementById("txtcorreo").value;
+        var pincidencia = document.getElementById("tipoincidencia").selectedIndex;
+
+        var pdescripcion = document.getElementById("txtDescripcion").value;
+      
+
+        if (pnombre == "") {
+            alert("Ingrese un nombre!");
+            return false;
+        }
+        if (ptelefono == "") {
+            alert("Ingrese un telefono!");
+            return false;
+        }
+        //if (pFullname == "") {
+        //    alert("Ingrese el nombre completo!");
+        //    return false;
+        //}
+        if (pincidencia == 0) {
+            alert("Ingrese una incidencia!");
+            return false;
+        }
+
+        var insert = 0;
+        if (confirm("Estas seguro?")) {
+
+
+           // sqlite()
+
+            var db = openDatabase('Database', '1.0', 'Alcaldes', 2 * 1024 * 1024);
+            var msg;
+            db.executeSql('SELECT * FROM id_incidencias1', [], function (tx, results) {
+                var len = results.rows.length, i;
+                msg = "<p>Found rows: " + len + "</p>";
+                document.querySelector('#status').innerHTML += msg;
+
+                for (i = 0; i < len; i++) {
+                    var ide = results.rows.item(i).id
+                    var platitud = results.rows.item(i).latitud
+                    var plongitud = results.rows.item(i).longitud
+                    msg = "<p><b>" + results.rows.item(i).id + "</b></p>";
+                    document.querySelector('#status').innerHTML += ide;
+                }
+            }, null);
+
+
+            db.transaction(function (tx) {
+
+                db.executeSql('CREATE TABLE  if not exists incidencias1  (id  TEXT NOT NULL PRIMARY KEY ,nombre    TEXT,telefono  TEXT,correo   TEXT,tipoIncidencia INTEGER,descripcion  TEXT,latitud  TEXT,longitud TEXT, fechaCaptura  TEXT,  sincronizado   INTEGER NOT NULL DEFAULT 0 )')
+
+                // tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)');
+
+
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth() + 1; //January is 0!
+                var h = today.getHours();
+                var m = today.getMinutes();
+                var s = today.getSeconds();
+                var yyyy = today.getFullYear();
+                if (dd < 10) {
+                    dd = '0' + dd;
+                }
+                if (mm < 10) {
+                    mm = '0' + mm;
+                }
+                var today = yyyy + '/' + mm + '/' + dd + '' + h + ':' + m + ':' + s;
+
+                ///
+                tx.executeSql('INSERT INTO incidencias1 (id, nombre,telefono,correo,tipoIncidencia,descripcion,idplaza,latitud,longitud,fechaCaptura,tipo,sincronizado) VALUES ("' + ide + '", "' + pnombre + '", "' + ptelefono + '", "' + pcorreo + '", "' + pcorreo + '", "' + pincidencia + '", "' + pdescripcion + '", "' + platitud + '", "' + plongitud +'")');
+              //  tx.executeSql('INSERT INTO LOGS (id, log) VALUES (2, "logmsg")');
+                msg = '<p>Se guardo correctamente.</p>';
+                document.querySelector('#status').innerHTML = msg;
+
+
+
+            })
+
+
+            //agregando evento Ajax
+            $.ajax({
+           
+                         type: "GET",
+                    url: "https://implementta.net/andro/AppIncidencias.aspx?query=sp_CargaIncidencia%20%27Edu1%27,%27edu.revilla.vaquero@gmail.com%27,%2712345%27,2,%27descr%27,1,%27195252%27,%27-5252525%27,%272018/01/01%27",
+                            crossDomain: true,
+                                cache: false,
+                                    contentType: "application/json; charset=utf-8",
+                                        async: false,
+                                            dataType: "JSON",
+                                            success: function (result) {
+                                                $.each(result,function(i, field) {
+                                                    insert = field.id;  });
+                    //if (result.d != null) {
+                    //    insert = result.d;
+                    //}
+
+                                                if (insert != "") {
+
+                        //var pnombre = document.getElementById("txtnombre").value;
+                        //var ptelefono = document.getElementById("txttelefono").value;
+                        //var pcorreo = document.getElementById("txtcorreo").value;
+                        //var pincidencia = document.getElementById("tipoincidencia").selectedIndex;
+
+                        //var pdescripcion = document.getElementById("txtDescripcion").value;
+
+
+                        document.getElementById("txtnombre").value = "";
+                        document.getElementById("txttelefono").value = "";
+                        document.getElementById("txtcorreo").value = "";
+                        document.getElementById("tipoincidencia").selectedIndex = "0";
+                    
+                        document.getElementById("txtDescripcion").value = "";
+                      ///  document.getElementById("txtFechaNac").value = "";
+                        $("#divRegistro").html("Registro creado satisfactoriamente!");
+                    } else {
+                        $("#divRegistro").html("Error al registrar el usuario!");
+                    }
+                },
+                error: function (result) {
+                    alert("Ocurrió un problema. Por favor Comuníquese con el administrador del sistema. Gracias.");
+                }
+            });
+        }
+    }
+
+
+    ///sql
+    function sqlite() {
+
+     //   var db = openDatabase('Database', '1.0', 'Alcaldes', 2 * 1024 * 1024);/// esta 
+        var db = window.openDatabase("Database", "1.0", "Alcaldes", 200000);//esta sirve para los select 
+        db.transaction(populateDB, errorCB, successCB);
+
+      
+      //  var url = "incidencia.html";
+     //   $(location).attr('href', url);
+    }
+
+
+
+    function populateDB(tx) {
+        ///select 
+      //  db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM id_incidencias1', [], function (tx, results) {
+                var len = results.rows.length, i;
+                msg = "<p>Found rows: " + len + "</p>";
+                document.querySelector('#status').innerHTML += msg;
+
+                for (i = 0; i < len; i++) {
+                    var ide = results.rows.item(i).id
+                    msg = "<p><b>" + results.rows.item(i).id + "</b></p>";
+                    document.querySelector('#status').innerHTML += ide;
+                }
+            }, null);
+      //  }); 
+
+
+
+        ////insert
+        //   var lat = document.getElementById("txtLat").value;
+        //var lng = document.getElementById("txtLon").value;
+        //tx.executeSql('DROP TABLE IF EXISTS id_incidencias1');
+
+        //tx.executeSql('CREATE TABLE   IF NOT EXISTS id_incidencias1 (id ,latitud  ,longitud  )');
+  
+
+        //var today = new Date();
+        //var dd = today.getDate();
+        //var mm = today.getMonth() + 1; //January is 0!
+        //var h = today.getHours();
+        //var m = today.getMinutes();
+        //var s = today.getSeconds();
+        //var yyyy = today.getFullYear();
+        //if (dd < 10) {
+        //    dd = '0' + dd;
+        //}
+        //if (mm < 10) {
+        //    mm = '0' + mm;
+        //}
+        //var today = yyyy + '/' + mm + '/' + dd + '' + h + ':' + m + ':' + s;
+        //var id = today + '' + lat + '' + lng
+        ////  var d = new Date();
+        ///////DELETE ANTES DE INSERTAR 
+        ////// EL ID SERA LA LATITUD LONGITUD Y FECHA HOY 
+        //tx.executeSql(`INSERT INTO id_incidencias1 VALUES("${id}","${lat}","${lng}")`);
+
+
+
+        //console.log(`INSERT INTO id_incidencias1 VALUES("${id}","${lat}","${lng}") `);
+      
+
+    }
+
+    // Transaction error callback
+    //
+    function errorCB(tx, err) {
+        alert("Error processing SQL: " + err);
+    }
+
+    // Transaction success callback
+    //
+    function successCB() {
+        alert("success!");
+    }
+
+})();
---
+++ www/scripts/sqlite.js
@@ -0,0 +1,230 @@
+﻿jQuery(document).ready(function () {
+    // se une el envío de formularios y campos para el motor de validación
+    jQuery("#formID").validationEngine();
+});
+
+
+(function () {
+    "use strict";
+    //sqwlite
+    //document.addEventListener("deviceready", onDeviceReady, false);
+    //
+    var myDB;
+
+    window.addEventListener('load', getDatos, false);
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    // getDatos
+    function onDeviceReady() {
+        jQuery(document).ready(function () {
+            // se une el envío de formularios y campos para el motor de validación
+            jQuery("#formID").validationEngine();
+        });
+
+        $('#btnMostrarMapa').click
+            
+            (sqlite);
+    };
+
+    function onPause() {
+        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
+    };
+
+    function onResume() {
+        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
+        // getDatos
+    };
+
+
+
+    ///sqlite 
+    function populateDB(tx) {
+
+
+        //// EL ID SERA LA LATITUD LONGITUD Y FECHA HOY 
+        ///
+
+
+        ///  var nombre = "Juan";
+        //  console.log(`¡Hola ${nombre}!`);
+        var lat = document.getElementById("txtLat").value;
+        var lng = document.getElementById("txtLon").value;
+        tx.executeSql('DROP TABLE IF EXISTS id_incidencias1');
+
+        tx.executeSql('CREATE TABLE   IF NOT EXISTS id_incidencias1 (id ,latitud  ,longitud  )');
+        // tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+        // var saludo = `¡\`Hola\` Mundo!`;
+        //  tx.executeSql(`INSERT INTO id_incidencias1 VALUES("17","${lat }","${lng }")`);
+
+
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+        var today = yyyy + '/' + mm + '/' + dd + '' + h + ':' + m + ':' + s;
+        var id = today + '' + lat + '' + lng
+        //  var d = new Date();
+        /////DELETE ANTES DE INSERTAR 
+        //// EL ID SERA LA LATITUD LONGITUD Y FECHA HOY 
+        tx.executeSql(`INSERT INTO id_incidencias1 VALUES("${id}","${lat}","${lng}")`);
+
+
+        ////PANTALLA 2 
+
+        // tx.executeSql('CREATE TABLE  if not exists incidencias1  (id  TEXT NOT NULL PRIMARY KEY ,nombre    TEXT,telefono  TEXT,correo   TEXT,tipoIncidencia INTEGER,descripcion  TEXT, idplaza  INTEGER,latitud  TEXT,longitud TEXT, fechaCaptura  TEXT, tipo INTEGER NOT NULL DEFAULT 1, sincronizado   INTEGER NOT NULL DEFAULT 0 )')
+
+
+
+
+        // tx.executeSql(`INSERT INTO id_incidencias1 VALUES("${id}","${lat}","${lng}") `);
+        //tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+        //tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+
+        console.log(`INSERT INTO id_incidencias1 VALUES("${id}","${lat}","${lng}") `);
+        // tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+
+
+    }
+
+    // Transaction error callback
+    //
+    function errorCB(tx, err) {
+        alert("Error processing SQL: " + err);
+    }
+
+    // Transaction success callback
+    //
+    function successCB() {
+        alert("success!");
+    }
+
+    function sqlite() {
+        var url = "sqlite.html";
+        $(location).attr('href', url);
+
+        var db = window.openDatabase("Database", "1.0", "Alcaldes", 200000);
+        db.transaction(populateDB, errorCB, successCB);
+
+        // navigator("sqlite.html")
+
+    }
+    ////
+    function getDatos() {
+
+
+
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+            maximunAge: 300000,
+            timeout: 10000,
+            enableHighAccuracy: true
+        });
+    }
+
+    function onSuccess(position) {
+        var cusLat = position.coords.latitude;
+        var cusLon = position.coords.longitude;
+
+        document.getElementById("txtLat").value = cusLat;
+        document.getElementById("txtLon").value = cusLon;
+        initialize()
+
+        //  try {
+        //      var coords = new google.maps.LatLng(cusLat, cusLon);
+
+        //      var opciones = {
+        //          center: coords, zoom: 15
+        //      };
+
+        //      var mapa = new google.maps.Map(document.getElementById("map"), opciones);
+        //      var marcador = new google.maps.Marker({
+        //          position: coords,
+        //          map: mapa,
+        //          title: "Mi ubicación",
+        //          animation: google.maps.Animation.DROP,
+        //           draggable:true
+        //      });
+
+
+        //      google.maps.event.addListener(marker, 'dragend', function (event) {
+        //          document.getElementById("txtLat").value = this.getPosition().toString();
+        //      });
+
+        //      var latlng = marker.getPosition();
+        //      document.getElementById("txtLat").value = latlng.latitude;
+        //      document.getElementById("txtLon").value = latlng.longitude;
+
+
+        //      var content = `
+        //<div id="myid"  class="item item-thumbnail-left item-text-wrap">
+        //  <ion-item>
+        //    <ion-row>
+        //      <h6>`+ marcador.title + `</h6>
+        //    </ion-row>
+        //  </ion-item>
+        //</div>
+        //`
+        //          ;
+        //      this.addInfoWindow(marcador, content);
+        //      marker.setMap(this.mapa);
+
+        //  }
+        //  catch (err) {
+        //      console.log(err.message);
+        //  }
+    }
+
+    var map;
+    function initialize() {
+        var cusLat = document.getElementById("txtLat").value;
+        var cusLon = document.getElementById("txtLon").value;
+
+        // document.getElementById("txtLat").value = cusLat;
+        // document.getElementById("txtLon").value = cusLon;
+
+        //  try {
+        var coords = new google.maps.LatLng(cusLat, cusLon);
+
+        var opciones = {
+            center: coords, zoom: 15
+        };
+
+        //  var mapa = new google.maps.Map(document.getElementById("map"), opciones);
+        map = new google.maps.Map(document.getElementById('map'), opciones);
+
+        var marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            draggable: true/// marcADOR MOVIBLE 
+
+        });
+
+
+        //FUNCION PARA OBTENER LA LATITUD Y LONG 
+        google.maps.event.addListener(marker, 'dragend', function (event) {
+            //  document.getElementById("txtLon").value = this.getPosition().toString();
+            //    document.getElementById("txtLat").value = this.getPosition().lat() + "," + this.getPosition().lng();
+
+            document.getElementById("txtLat").value = this.getPosition().lat();
+            document.getElementById("txtLon").value = this.getPosition().lng();
+        });
+        //   document.getElementById("txtLat").value=this.getPosition();
+        //  document.getElementById("txtLon").value = this.getPosition.longitude;
+        //  });
+    }
+
+
+    //  google.maps.event.addDomListener(window, 'load', initialize);
+    function onError(err) {
+        console.log("codigo de err:" + err.code + "  msj=" + err.message);
+    }
+})();
